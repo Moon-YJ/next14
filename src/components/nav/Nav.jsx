5d616cfc886ab0c269c49cca20e777f2a8cf3e18
Moon-YJ <tunjeong@gmail.com>
2024-01-15 14:57:43 +0900

    hydration 오류 해결방법2(dynamic import로 특정 클라이언트 컴포넌트 ssr에서 제외)

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@
 // hydration: 정적인 데이터를 우선 기능없이 미리 서버쪽에서 pre-render해서 출력한 다음, 클라이언트가 동작할 준비가 되면 그때 클라이언트 기능을 활용할 수 있는 동적인 컴포넌트로 변경 처리
 // 주의할 점: 서버쪽에서 랜더링된 결과값과 초기 클라이언트에서 동작되는 값이 동일해야됨
 // 해결방법1: useEffect로 컴포넌트가 마운트됐을때만 클라이언트에서 활용한 값을 호출
+// 해결방법2: Dynamic import방식을 활용해서 client 방식으로 동작하는 컴포넌트를 애초에 서버쪽에서 prebuild 되지 않도록 처리
 
 import clsx from 'clsx';
 import Link from 'next/link';
@@ -15,16 +16,16 @@ export default function Nav({ pages }) {
 	console.log('nav'); // 브라우저 콘솔창에 찍힘(nav만 동적으로 가져옴)
 	const time = new Date().getTime();
 	console.log(time, '::time');
-	const [Client, setClient] = useState(false);
+	//const [Client, setClient] = useState(false);
 
-	useEffect(() => {
-		setClient(true);
-	}, []);
+	// useEffect(() => {
+	// 	setClient(true);
+	// }, []);
 
 	return (
 		<nav className={clsx(styles.nav)}>
 			{/* 해결방법1 */}
-			<h2>{Client && time}</h2>
+			<h2>{time}</h2>
 			{pages.map(page => (
 				<Link key={page} href={`/${page}`} className={clsx(pathName === `/${page}` ? styles.on : '')}>
 					{page.charAt(0).toUpperCase() + page.slice(1)}
