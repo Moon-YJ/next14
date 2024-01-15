bbdd74f40dea38f4272801ede0d86e0f35ca272f
Moon-YJ <tunjeong@gmail.com>
2024-01-15 16:49:06 +0900

    header의 모바일 메뉴 반응형 처리

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import styles from './header.module.scss';
 import Link from 'next/link';
 import Nav from '../nav/Nav.jsx';
 import BtnLogin from '../btnLogin/BtnLogin';
+import { FaBarsStaggered } from 'react-icons/fa6';
 //import dynamic from 'next/dynamic';
 
 // 해결방법2: 기존 client 방식의 컴포넌트 import시 ssr: false를 통해 서버쪽에서 pre-build 되지 않도록 설정 (build 자체를 하지 않아서 사용자가 빈 화면을 보게 되므로 해결방법1 사용 권장)
@@ -19,6 +20,7 @@ export default function Header() {
 			<Nav pages={['about', 'youtube', 'post']} />
 			{/* <NoSsrNav pages={['about', 'youtube', 'post']} /> */}
 			<BtnLogin session={true} />
+			<FaBarsStaggered color='#333' className={clsx(styles.btnMenu)} />
 		</header>
 	);
 }
