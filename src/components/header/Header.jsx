f5ad99e068dff67e7deed5fbf782eeed8a87c096
Moon-YJ <tunjeong@gmail.com>
2024-01-15 12:55:22 +0900

    Header 컴포넌트에 일일이 Link로 연결하는 경우

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,29 @@
+'use client';
 import clsx from 'clsx';
 import styles from './header.module.scss';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
-	return <header className={clsx(styles.header)}></header>;
+	const pathName = usePathname();
+	console.log(pathName);
+	return (
+		<header className={clsx(styles.header)}>
+			<h1>
+				<Link href='/'>ABC Company</Link>
+			</h1>
+
+			<nav>
+				<Link href='/about' className={clsx(pathName === '/about' ? styles.on : '')}>
+					About
+				</Link>
+				<Link href='/youtube' className={clsx(pathName === '/youtube' ? styles.on : '')}>
+					Youtube
+				</Link>
+				<Link href='/post' className={clsx(pathName === '/post' ? styles.on : '')}>
+					Post
+				</Link>
+			</nav>
+		</header>
+	);
 }
