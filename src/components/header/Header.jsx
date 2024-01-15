e1e5b23a961c7067f77ad2335def22022b0d4c8f
Moon-YJ <tunjeong@gmail.com>
2024-01-15 15:41:16 +0900

    loading 컴포넌트 추가하여 라우터 이동시 해당 컴포넌트 출력

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import styles from './header.module.scss';
 import Link from 'next/link';
 import Nav from '../nav/Nav.jsx';
+import BtnLogin from '../btnLogin/BtnLogin';
 //import dynamic from 'next/dynamic';
 
 // 해결방법2: 기존 client 방식의 컴포넌트 import시 ssr: false를 통해 서버쪽에서 pre-build 되지 않도록 설정 (build 자체를 하지 않아서 사용자가 빈 화면을 보게 되므로 해결방법1 사용 권장)
@@ -18,6 +19,7 @@ export default function Header() {
 			<nav>
 				<Nav pages={['about', 'youtube', 'post']} />
 				{/* <NoSsrNav pages={['about', 'youtube', 'post']} /> */}
+				<BtnLogin session={false} />
 			</nav>
 		</header>
 	);
