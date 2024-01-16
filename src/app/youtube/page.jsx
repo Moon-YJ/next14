fa3d179ec9398f5491c1823b790e713a8ce904bc
Moon-YJ <tunjeong@gmail.com>
2024-01-16 12:10:07 +0900

    초반 4개 카드 컴포넌트의 이미지만 priority설정, 그 이후 컴포넌트는 lazyLoading되도록 처리

--- src/app/youtube/page.jsx
+++ src/app/youtube/page.jsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import styles from './youtube.module.scss';
-import { useCustomText } from '@/hooks/useText';
-import Image from 'next/image';
+import YoutubeCard from '@/components/youtubeCard/YoutubeCard';
 
 async function fetchYoutube() {
 	const api_key = 'AIzaSyB81cXmxoWdzbYs8QZUlN_LQskZFT_Xqoo';
@@ -14,39 +13,13 @@ async function fetchYoutube() {
 }
 
 export default async function Youtube() {
-	const shortenText = useCustomText('shorten');
-	const customText = useCustomText('combined');
 	const data = await fetchYoutube();
-	console.log(data);
 	return (
 		<section className={clsx(styles.youtube)}>
 			<h1>Youtube</h1>
 			{data.items.map((data, idx) => {
-				const [date, time] = data.snippet.publishedAt.split('T');
-
-				return (
-					<article key={data.id + idx}>
-						<div className={styles.pic}>
-							{/* 외부 이미지 연결시 next.config.js파일에 이미지 protocol, hostname 등록해야함(next remotepatterns 참고), fill, sizes, priority도 등록해야함 */}
-							{/* fill속성 적용시 무조건 부모요소에 position: relative, absolute, fixed 설정되어있어야함 */}
-							<Image
-								src={data.snippet.thumbnails.standard.url}
-								fill
-								sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-								priority
-								alt={data.snippet.title}
-							/>
-						</div>
-						<h2>{shortenText(data.snippet.title, 50)}</h2>
-						<div className={styles.txt}>
-							<p>{shortenText(data.snippet.description, 250)}</p>
-							<div className={styles.infoBox}>
-								<span>{customText(date, '.')}</span>
-								{/* <em>{time.split('Z')[0]}</em> */}
-							</div>
-						</div>
-					</article>
-				);
+				if (idx < 4) return <YoutubeCard key={data.id} data={data} isPriority={true} />;
+				return <YoutubeCard key={data.id} data={data} isPriority={false} />;
 			})}
 		</section>
 	);
---
+++ src/components/youtubeCard/YoutubeCard.jsx
@@ -0,0 +1,46 @@
+import clsx from 'clsx';
+import styles from './youtubeCard.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useCustomText } from '@/hooks/useText';
+
+export default function YoutubeCard({ data, isPriority }) {
+	const shortenText = useCustomText('shorten');
+	const customText = useCustomText('combined');
+	const [date, _] = data.snippet.publishedAt.split('T');
+
+	return (
+		<article className={clsx(styles.youtubeCard)}>
+			<div className={styles.pic}>
+				{/* 외부 이미지 연결시 next.config.js파일에 이미지 protocol, hostname 등록해야함(next remotepatterns 참고), fill, sizes, priority도 등록해야함 */}
+				{/* fill속성 적용시 무조건 부모요소에 position: relative, absolute, fixed 설정되어있어야함 */}
+				<Link href={`/detail/${data.id}`}>
+					{isPriority ? (
+						<Image
+							src={data.snippet.thumbnails.standard.url}
+							alt={data.snippet.title}
+							fill
+							sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+							priority
+						/>
+					) : (
+						<Image
+							src={data.snippet.thumbnails.standard.url}
+							alt={data.snippet.title}
+							fill
+							sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+						/>
+					)}
+				</Link>
+			</div>
+			<h2>{shortenText(data.snippet.title, 50)}</h2>
+			<div className={styles.txt}>
+				<p>{shortenText(data.snippet.description, 250)}</p>
+				<div className={styles.infoBox}>
+					<span>{customText(date, '.')}</span>
+					{/* <em>{time.split('Z')[0]}</em> */}
+				</div>
+			</div>
+		</article>
+	);
+}
