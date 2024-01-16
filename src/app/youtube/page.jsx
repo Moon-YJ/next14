a9b929a2ef4ee69f7aa66851526013762f4ce0b6
Moon-YJ <tunjeong@gmail.com>
2024-01-16 10:57:42 +0900

    Youtube 컴포넌트 data fetching(Image 오류)

--- src/app/youtube/page.jsx
+++ src/app/youtube/page.jsx
@@ -1,10 +1,56 @@
 import clsx from 'clsx';
 import styles from './youtube.module.scss';
+import { useCustomText } from '@/hooks/useText';
+import Link from 'next/navigation';
+import Image from 'next/image';
 
-export default function Youtube() {
+async function fetchYoutube() {
+	const api_key = 'AIzaSyB81cXmxoWdzbYs8QZUlN_LQskZFT_Xqoo';
+	const pid = 'PLMaY0ixOiylhwpATMD-PQjeVREn5MOfSp';
+	const num = 7;
+	const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+	const data = await fetch(baseURL);
+	const json = await data.json();
+	return json;
+}
+
+export default async function Youtube() {
+	const shortenText = useCustomText('shorten');
+	const customText = useCustomText('combined');
+	const data = await fetchYoutube();
+	console.log(data);
 	return (
 		<section className={clsx(styles.youtube)}>
 			<h1>Youtube</h1>
+			{data.items.map((data, idx) => {
+				const [date, time] = data.snippet.publishedAt.split('T');
+
+				return (
+					<article key={data.id + idx}>
+						<h2>{shortenText(data.snippet.title, 50)}</h2>
+
+						<div className='txt'>
+							<p>{shortenText(data.snippet.description, 250)}</p>
+							<div className='infoBox'>
+								<span>{customText(date, '.')}</span>
+								<em>{time.split('Z')[0]}</em>
+							</div>
+						</div>
+
+						<div className='pic'>
+							{/* <Image
+								width={150}
+								height={100}
+								src={data.snippet.thumbnails.standard ? data.snippet.thumbnails.standard.url : '/img/member1.jpg'}
+								alt={data.snippet.title}
+							/> */}
+							{/* <Link to={`/detail/${data.id}`}>
+								<Image src={data.snippet.thumbnails.standard ? data.snippet.thumbnails.standard.url : '/img/member1.jpg'} alt={data.snippet.title} />
+							</Link> */}
+						</div>
+					</article>
+				);
+			})}
 		</section>
 	);
 }
