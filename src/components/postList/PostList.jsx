2fa4d563db99b18a35620a4ace408b92d73a7e13
Moon-YJ <tunjeong@gmail.com>
2024-01-31 23:35:48 +0900

    페이지별 로딩처리

--- src/components/postList/PostList.jsx
+++ src/components/postList/PostList.jsx
@@ -6,7 +6,9 @@ import Image from 'next/image';
 import { getPostsPage } from '@/lib/actions';
 
 export default async function PostList({ searchParams }) {
+	// 현재 페이지에 전달된 query가 있으면 page의 값을 가져옴, 없으면 1로 초기화
 	const page = searchParams?.page || 1;
+	// 현재 페이지 번호를 인수로 전달해서 페이지번호에 따른 전체 포스트 개수, 출력할 포스트배열, 페이지당 출력할 개수 반환
 	const { total, posts, nums } = await getPostsPage(page);
 
 	return (
