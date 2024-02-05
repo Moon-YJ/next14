2456fe84377b28a4d3575308c61015a97c850169
Moon-YJ <tunjeong@gmail.com>
2024-02-05 21:49:10 +0900

    (auth) join, login 컴포넌트 관련 주석 추가

--- src/app/(auth)/join/page.jsx
+++ src/app/(auth)/join/page.jsx
@@ -10,3 +10,9 @@ export default function Join() {
 		</section>
 	);
 }
+
+/*
+	Skip 라우터로 중간 url을 생략처리하고 회원관련 페이지 컴포넌트 생성
+	- 회원가입 이벤트는 클라이언트 방식으로 처리해야 하므로 JoinMembers를 따로 클라이언트 방식으로 컴포넌트 분리해서 호출
+	- JoinMembers.jsx 파일 참조
+*/
--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -16,3 +16,15 @@ export default function Login() {
 		</section>
 	);
 }
+
+/*
+	skip 라우터로 불필요한 url path경로 생략하고 로그인 페이지 컴포넌트 생성
+	- github, google 같이 외부 서비스 로그인 기능은 actions.js의 서버액션 함수를 연결
+	- 일반 로그인 이벤트는 클라이언트 방식으로 동작해야 함으로 따로 클라이언트 방식 컴포넌트를 제작해서 연동
+
+	handleGithubLogin, handleGoogleLogin
+	- lib > actions.js에서 서버액션 함수 참조
+	
+	LoginForm
+	- components > LoginForm.jsx에서 참조
+*/
